3401f6ebb66e7f2dd6022cb8bdfe777ce508b7cb
Sabbir Ahmad <sabbirahmad00521@gmail.com>
2019-06-27 16:38:55 +0600

    update

---
+++ 93. infinity (60$ + 20) 22 may 2019/Your Website/assets/js/scripts.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function ($) {
+
+   // Owl Carousel
+   
+   $('.slider').owlCarousel({
+      items: 1,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      dots: true
+   });
+   
+   
+   // Other Scripts
+
+
+});
